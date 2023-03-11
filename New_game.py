8209a5745f4aa45bac7604858b02e30c664e7b30
Holocron <108415700+Ho1ocron@users.noreply.github.com>
2023-03-11 20:08:32 +0500

    Update New_game.py

--- New_game.py
+++ New_game.py
@@ -1,8 +1,10 @@
 import pygame
+import random
 import sys
 
 all_sprites = pygame.sprite.Group()
 enemy = pygame.sprite.Group()
+bullet = pygame.sprite.Group()
 playerg = pygame.sprite.Group()
 
 
@@ -15,7 +17,7 @@ class Player(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.rect = pygame.Rect(self.x, self.y, 20, 20)
-        pygame.draw.rect(self.image, self.color, self.rect, 3)
+        pygame.draw.rect(self.image, self.color, self.rect, 1)
         self.add(playerg)
     
     def move(self, evx, evy):
@@ -28,13 +30,48 @@ class Player(pygame.sprite.Sprite):
             while self.x > x and x <= 685:
                 self.x -= 1
                 self.rect = self.rect.move(-1, 0)
-            while self.y < y and y <= 685:
+            while self.y < y and y <= 685 and y >= 400:
                 self.y += 1
                 self.rect = self.rect.move(0, 1)
-            while self.y > y and y <= 685:
+            while self.y > y and y <= 685 and y >= 400:
                 self.y -= 1
                 self.rect = self.rect.move(0, -1)
+    def shoot(self):
+        Bullet(self.x, self.y)
+    
 
+class Bullet(pygame.sprite.Sprite): # падающий объект
+    def __init__(self, x: int, y: int):
+        super().__init__(all_sprites)
+        self.image = pygame.Surface((5, 15))
+        self.color = pygame.Color((204, 0, 102))
+        self.image.fill(self.color)
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(self.x, self.y, 5, 15)
+        pygame.draw.rect(self.image, self.color, self.rect, 3)
+        self.add(bullet)
+
+    def update(self):
+        self.rect = self.rect.move(0, -5) # иначе продолжает падать
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = pygame.Surface((30, 30))
+        self.color = pygame.Color((153, 0, 76))
+        self.image.fill(self.color)
+        self.rect = pygame.Rect(random.randint(10, 650), 0, 30, 30)
+        pygame.draw.rect(self.image, self.color, self.rect, 3)
+    
+    def update(self):
+        if pygame.sprite.spritecollideany(self, playerg):
+            self.kill()
+        elif pygame.sprite.spritecollideany(self, bullet):
+            self.kill()
+        else:
+            self.rect = self.rect.move(0, 1)
 
 
 pygame.init()
@@ -45,10 +82,12 @@ pygame.display.set_caption("Chippin' In")
 screen.fill(BLACK)
 fps = 120
 clock = pygame.time.Clock()
+
 player1 = Player(110, 510)
 
+
 while True:
-    screen.fill('black')
+    screen.fill("black")
     all_sprites.draw(screen)
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -59,7 +98,12 @@ while True:
                 _y = event.pos[1]
                 player1.move(_x, _y)
                 #print(event.pos[0])
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    player1.shoot()
+    Enemy()
     all_sprites.update()
     clock.tick(fps)
     pygame.display.flip()
+
 
